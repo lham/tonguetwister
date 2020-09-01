433635d024cafcfff6fe15c89ba2b622eb47d7ba
Linus Hammarlund <linus.hammarlund@gmail.com>
2020-09-01 20:00:36 +0200

    Update example file

--- example.py
+++ example.py
@@ -1,29 +1,31 @@
-"""
-Example useage of the TongueTwister module.
-
-Call with:
-  python example.py <link/to/folder/data.cst> <script_number_to_decompile> <function_number_to_decompile>
-"""
-import sys
+import argparse
 from tonguetwister.parser import RifxParser
 from tonguetwister.lingo_decompiler import Decompiler
 
-def main(filename, script_number, function_number):
-  # Unpack a cst file.
-  data = RifxParser(filename)
-  data.unpack()
 
-  # Extract a specific lingo script
-  script = data.lingo_scripts.items()[script_number][1]
-  namelist = data.namelist
-  function = script.functions[function_number]
+def main(filepath, script_number, function_number):
+    # Unpack a cst file.
+    data = RifxParser(filepath)
+    data.unpack()
 
-  # Decompile the lingo script
-  Decompiler().twist_to_pseudo(function, namelist, script)
+    # Extract a specific lingo script
+    script = data.lingo_scripts.items()[script_number][1]
+    namelist = data.namelist
+    function = script.functions[function_number]
+
+    # Decompile the lingo script
+    Decompiler().to_pseudo_code(function, namelist, script)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', type=str, help='A director data file path, e.g. link/to/folder/data.cst')
+    parser.add_argument('script_number', metavar='script-number', type=int, help='The script number to decompile')
+    parser.add_argument('function_number', metavar='function-number', type=int, help='The function number to decompile')
+
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-  filename = sys.argv[1]
-  script_number = int(sys.argv[2])
-  function_number = int(sys.argv[3])
-
-  main(filename, script_number, function_number)
+    args = parse_args()
+    main(args.filepath, args.script_number, args.function_number)
