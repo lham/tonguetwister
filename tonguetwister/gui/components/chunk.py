1141f36598af1148ace37c96a53c2bae18576a82
Linus Hammarlund <linus.hammarlund@gmail.com>
2020-10-30 23:03:57 +0100

    Update generic chunk textual representation

--- tonguetwister/gui/components/chunk.py
+++ tonguetwister/gui/components/chunk.py
@@ -1,15 +1,15 @@
 import os
-from collections import OrderedDict
 
 from kivy.uix.textinput import TextInput
 
 from tonguetwister.file_disassembler import FileDisassembler
 from tonguetwister.gui.utils import scroll_to_top
-from tonguetwister.lib.helper import splat_ordered_dict, splat_list
+from tonguetwister.lib.helper import splat_ordered_dict
 
 
 class DefaultChunkView(TextInput):
     key_width = 40
+    number_width = 6
     indent = '    '
     separator = f'{os.linesep}{indent}'
 
@@ -21,31 +21,54 @@ class DefaultChunkView(TextInput):
         self.text = (
             f'Chunk type: {chunk.__class__.__name__} (FOUR CC: {chunk.four_cc})'
             + self._section_string('Header')
-            + self._splat_dict(chunk.header)
+            + self._format_value(chunk.header)
             + self._section_string('Body')
-            + self._splat_dict(chunk.body)
+            + self._format_value(chunk.body)
             + self._section_string('Footer')
-            + self._splat_dict(chunk.footer)
+            + self._format_value(chunk.footer)
         )
         scroll_to_top(self)
 
     @staticmethod
     def _section_string(title):
-        return (
-            f'\n\n'
-            f'============================\n'
-            f'{title}:\n'
-            f'============================\n\n'
-        )
+        return os.linesep.join([
+            os.linesep,
+            '============================',
+            str(title),
+            '============================',
+            os.linesep
+        ])
 
-    def _splat_dict(self, d):
-        if d is None or len(d) == 0:
-            return f'{self.indent}None'
+    def _format_value(self, value, depth=1):
+        lines = []
+        indent = self.indent * depth
 
-        if isinstance(d, OrderedDict):
-            return self.indent + splat_ordered_dict(d, self.separator, self.key_width)
-        elif isinstance(d, (list, tuple)):
-            return self.indent + splat_list(d, self.separator, self.key_width)
+        if value is None:
+            lines.append(indent + 'None')
+        elif isinstance(value, dict):
+            for key, _value in value.items():
+                lines.append(f'{indent}{str(key):.<{self.key_width}s}: {self._value_to_line(_value, depth)}')
+        elif isinstance(value, (list, tuple)):
+            for i, _value in enumerate(value):
+                lines.append(f'{indent}[{i}]: {self._value_to_line(_value, depth, True)}')
+        else:
+            print(f'WARNING: Not displaying chunk data due to bad type: {type(value)}')
+
+        return os.linesep.join(lines)
+
+    def _value_to_line(self, value, depth, from_list=False):
+        if isinstance(value, (dict, list, tuple)) and len(value) == 0:
+            return ''
+        if isinstance(value, (dict, list, tuple)):
+            return os.linesep + self._format_value(value, depth + 1)
+        elif isinstance(value, int) and from_list:
+            return f'{"." * self.key_width}: {value:{self.number_width}d} (0x{value:08x})'
+        elif isinstance(value, int):
+            return f'{value:{self.number_width}d} (0x{value:08x})'
+        elif from_list:
+            return f'{"." * self.key_width}: {value}'
+        else:
+            return value
 
 
 class DefaultRecordsChunkView(DefaultChunkView):
@@ -53,13 +76,13 @@ class DefaultRecordsChunkView(DefaultChunkView):
         self.text = (
             f'Chunk type: {chunk.__class__.__name__} (FOUR CC: {chunk.four_cc})'
             + self._section_string('Header')
-            + self._splat_dict(chunk.header)
+            + self._format_value(chunk.header)
             + self._section_string('Body')
-            + self._splat_dict(chunk.body)
+            + self._format_value(chunk.body)
             + self._section_string('Records')
             + self._display_records(chunk.records)
             + self._section_string('Footer')
-            + self._splat_dict(chunk.footer)
+            + self._format_value(chunk.footer)
         )
         scroll_to_top(self)
 
